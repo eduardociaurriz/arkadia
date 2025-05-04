64fd118a9a2048bf1a50661bcb2b1b6d18beb7a3
Eduardo <eduardo.ciaurriz.munuce@gmail.com>
2025-05-04 19:31:25 +0200

    Primera prueba

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
           priority
         />
       </div>
+      <div>Caca</div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
